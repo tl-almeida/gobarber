b8f7a96e0f14d4e0a262f0bd1c97954569b00a75
tl-almeida <thiagolimadealmeid4@gmail.com>
2020-03-05 15:44:49 -0300

    mailing configs

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '62a42500167278',
+        pass: '553a13c16aecf2'
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com'
+    }
+};
